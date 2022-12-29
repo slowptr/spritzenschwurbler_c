356d4b7f66be6494d73b0ff949fb09bd0fd6a52b
slowptr <42156104+slowptr@users.noreply.github.com>
2022-12-29 14:47:06 +0100

    Add sample GUI design

--- src/ss_window.c
+++ src/ss_window.c
@@ -1,5 +1,6 @@
 #include "ss_window.h"
 #include "utils/kippfenster/kf_wnd.h"
+#include "utils/kippfenster/kf_wnd_ctrls.h"
 
 kf_wnd_t ss_window;
 LRESULT CALLBACK
@@ -16,6 +17,27 @@ ss_window_wndproc (HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
     }
   return DefWindowProc (hwnd, msg, wparam, lparam);
 }
+void
+ss_window_populate_items ()
+{
+  const int padding = 10;
+
+  RECT rect;
+  GetClientRect (ss_window.hwnd, &rect);
+
+  int cb1_size[2] = { 222, 22 };
+  kf_wnd_ctrls_add_combobox (&ss_window, "cb1",
+                             (int[2]){
+                                 padding,
+                                 padding,
+                             },
+                             cb1_size);
+
+  kf_wnd_ctrls_add_button (
+      &ss_window, "btn1", "inject",
+      (int[2]){ padding + cb1_size[0] + (padding / 2), padding },
+      (int[2]){ 100, 22 });
+}
 
 bool
 ss_window_init ()
@@ -27,10 +49,12 @@ ss_window_init ()
   DWORD style = WS_OVERLAPPED | WS_SYSMENU;
   ss_window = kf_wnd_create (wnd_class, "spritzenschwurbler_c", style,
                              (int[2]){ CW_USEDEFAULT, CW_USEDEFAULT },
-                             (int[2]){ 400, 150 });
+                             (int[2]){ 365, 80 });
 
   if (!kf_wnd_init (&ss_window))
     return false;
+
+  ss_window_populate_items ();
 
   return true;
 }
